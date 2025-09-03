3d71887063d20c43c2c6e4100bce63a6d90e2e93
Msrliao <88956880+Msrliao@users.noreply.github.com>
2025-09-03 23:00:35 +0800

    Add src/components/HelloWorld.vue file

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code>
+      to test hot module replacement
+    </p>
+  </div>
+
+  <p class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
